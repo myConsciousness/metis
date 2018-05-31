bc2d60c6f3aded182a1d1989b7d923d012f4a759
myConsciousness <yourdream28@gmail.com>
2018-06-01 00:04:10 +0900

    fixed

--- docs/bak/conf.py
+++ docs/bak/conf.py
@@ -17,6 +17,7 @@ import sys
 import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../metis'))
 
+extensions = ["sphinx.ext.autodoc"]
 
 
 # -- Project information -----------------------------------------------------
@@ -40,7 +41,6 @@ release = '2018/05/31'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
